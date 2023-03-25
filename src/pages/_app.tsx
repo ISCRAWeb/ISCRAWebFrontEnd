11777d870617c466864d75dd861bda23462d4bba
Konkina Alina <konkinaan@student.bmstu.ru>
2023-03-25 11:59:22 +0300

    Added uikit without fast-refresh

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,15 +6,14 @@ import { useCallback, useEffect } from 'react';
 import Icons from 'uikit/dist/js/uikit-icons';
 
 export default function App({ Component, pageProps }: AppProps) {
-  
   const importUIkit = useCallback(async () => {
     const UIkit = (await import('uikit')).default;
     UIkit.use(Icons);
-  },[]);
+  }, []);
 
   useEffect(() => {
     importUIkit();
-  },[importUIkit]);
+  },[]);
 
 /*useEffect(() => {
     UIkit.use(Icons)}, []
